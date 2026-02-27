feb55f8e84e10b13cdb292fe27bdde8c783d9441
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-02-27 18:20:53 -0500

    Add some more code to displayBuffer

--- renderer.asm
+++ renderer.asm
@@ -24,6 +24,21 @@ displayBuffer PROC USES esi edi ecx ebx
 	mov esi, eax; // move source frame to esi
 	mov edi, OFFSET outputTextBuffer ;// move destination text buffer to edi
 
+	xor ebx, ebx ;// y_index = 0
+
+y_loop:
+	cmp ebx, SCREEN_HEIGHT
+	jge done
+
+	xor ecx, ecx ;// x_index = 0
+x_loop:
+	cmp ecx, SCREEN_WIDTH
+	jge row_end
+
+row_end:
+	jmp y_loop
+
+done: 
 	ret
 displayBuffer ENDP
 
