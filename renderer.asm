e1ea951216c956cb79e188e779693aba9aaf067a
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-02-27 17:26:38 -0500

    Add beginning of displayBuffer PROC

--- renderer.asm
+++ renderer.asm
@@ -6,9 +6,25 @@ option casemap : none
 include engine_types.inc
 
 .data
-screenBuffer Pixel 80 * 25 dup(<0, 0, 0, 255>)
+screenBuffer Pixel SCREEN_WIDTH * SCREEN_HEIGHT dup(<0, 0, 0, 255>)
 
 .code
+; // ----------------------------------
+; // displayBuffer
+; // Renders an RGBA buffer to the screen using ASCII characters.
+; // Intended to be used for frame by frame animation. 
+; // This will render on top of the last contents of screenBuffer.
+; // 
+; // Parameters: 
+; //	EAX - pointer to the new frame buffer to render. 
+; //
+; // Registers Clobbered: EAX
+; // ----------------------------------
+
+displayBuffer PROC
+
+displayBuffer ENDP
+
 main PROC
 xor eax, eax
 main ENDP
