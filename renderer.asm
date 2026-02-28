05416a97ed727599f7b6e300b734542183e5745b
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-02-28 10:02:33 -0500

    Add windows terminal calls

--- renderer.asm
+++ renderer.asm
@@ -4,8 +4,14 @@ option casemap : none
 .stack 4096
 
 include engine_types.inc
+SetConsoleOutputCP PROTO STDCALL : DWORD
+SetConsoleMode     PROTO STDCALL : DWORD, : DWORD
+GetStdHandle       PROTO STDCALL : DWORD
 
 .data
+STD_OUTPUT_HANDLE EQU -11
+ENABLE_VIRTUAL_TERMINAL_PROCESSING EQU 4
+
 CR = 13 ; // Carriage return
 LF = 10 ; // Line feed
 screenBuffer Pixel SCREEN_WIDTH * SCREEN_HEIGHT dup(<0, 0, 0, 255>)
@@ -24,18 +30,38 @@ outputTextBuffer db 100000 dup(0); // Used for the displayBuffer PROC
 ; // ----------------------------------
 displayBuffer PROC USES esi edi ecx ebx
 	mov esi, eax; // move source frame to esi
-	mov edi, OFFSET outputTextBuffer ;// move destination text buffer to edi
+	mov edi, OFFSET outputTextBuffer ; // move destination text buffer to edi
 
-	xor ebx, ebx ;// y_index = 0
+	xor ebx, ebx ; // y_index = 0
 
 y_loop:
 	cmp ebx, SCREEN_HEIGHT
 	jge done
 
-	xor ecx, ecx ;// x_index = 0
+	xor ecx, ecx ; // x_index = 0
 x_loop:
 	cmp ecx, SCREEN_WIDTH
 	jge row_end
+
+	; // Get top pixel
+	; // ((y_index * SCREENWIDTH) + x_index) * 2
+	mov eax, ebx
+	imul eax, SCREEN_WIDTH
+	add eax, ecx 
+	shl eax, 2
+	mov edx, [esi + eax] ; // edx is now the top pixel
+
+	; // Get bottom pixel
+	; // (((y_index + 1) * SCREENWIDTH) + x_index) * 2
+	mov eax, ebx
+	inc eax
+	imul eax, SCREEN_WIDTH
+	add eax, ecx
+	shl eax, 2
+	mov eax, [esi + eax] ; // eax is now the bottom pixel
+
+	; // Write character
+
 
 row_end:
 	; // Add newline
@@ -53,7 +79,15 @@ done:
 	ret
 displayBuffer ENDP
 
+; // This main method is for testing the renderer
 main PROC
+	; // Force output to be UTF-8
+	invoke SetConsoleOutputCP, 65001 
+
+	; // Enable virutal terminal processing (Required for RGB functionality)
+	invoke GetStdHandle, STD_OUTPUT_HANDLE
+	invoke SetConsoleMode, eax, ENABLE_VIRTUAL_TERMINAL_PROCESSING
+
 	xor eax, eax
 	mov eax, OFFSET screenBuffer
 	call displayBuffer
