7533bc02ebd9abbc57be87a929580231e8ed4481
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-10 18:55:05 -0400

    Add more detail to comment in main.asm

--- main.asm
+++ main.asm
@@ -13,6 +13,8 @@ ExitProcess PROTO STDCALL : DWORD
 .code
 main PROC PUBLIC
 	; // Eventually, this is where the main loop and heart of the engine will be.
+	; // This loop will update at a fixed 30 frames per second and call scene_update
+	; // on the current scene.
 	invoke ExitProcess, 0
 main ENDP
 
