0a528c0be75d3bbf6d0e9a9ad2840e7a7c5bb08b
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-10 18:53:43 -0400

    Add comments to main.asm

--- main.asm
+++ main.asm
@@ -1,10 +1,19 @@
+; // ==================================
+; // main.asm
+; // ----------------------------------
+; // The entry point of the program. Right now it is nonfunctional, but main.asm
+; // will be responsible for updating the currently running scene and the inialization
+; // of the engine.
+; // ==================================
+
 INCLUDE default_header.inc
 
 ExitProcess PROTO STDCALL : DWORD
 
 .code
 main PROC PUBLIC
-invoke ExitProcess, 0
+	; // Eventually, this is where the main loop and heart of the engine will be.
+	invoke ExitProcess, 0
 main ENDP
 
 END main
