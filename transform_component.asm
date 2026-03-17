bc8270a6c192acf3e00f9823e8d9cb59bbe07a8f
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-17 11:06:34 -0400

    Add transform_component.asm

---
+++ transform_component.asm
@@ -0,0 +1,16 @@
+; // ==================================
+; // TransformComponent
+; // ----------------------------------
+; // Defines where in space a GameObject should be
+; // represented.
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE transform_component.inc
+
+.code
+new_transform PROC
+
+new_transform ENDP
+
+END
