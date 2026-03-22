8dcebc6d2ea6d0799029cc32338888a593128783
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-22 18:46:10 -0400

    Implement new_rect_component

--- rect_component.asm
+++ rect_component.asm
@@ -12,13 +12,25 @@ INCLUDE heap_functions.inc
 
 .code
 new_rect_component PROC PUBLIC, h: DWORD, w : DWORD, r : BYTE, g : BYTE, b : BYTE, a : BYTE
-	mov eax, h
-	mov eax, w
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF RectComponent
+
+	; // Height & Width
+	mov esi, h
+	mov (RectComponent PTR [eax]).h, esi
+	mov esi, w
+	mov(RectComponent PTR[eax]).w, esi
+
+	; // RGBA
 	mov al, r
+	mov(RectComponent PTR[eax]).r, al
 	mov al, g
+	mov(RectComponent PTR[eax]).g, al
 	mov al, b
+	mov(RectComponent PTR[eax]).b, al
 	mov al, a
-	ret
+	mov(RectComponent PTR[eax]).a, al
+
+	ret ; // Return with the address of the memory block in HeapAlloc
 new_rect_component ENDP
 
 free_rect_component PROC PUBLIC, pRect: DWORD
