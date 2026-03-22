b3eb18e4c6d3e26da93ec16feea65120e14440fe
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-22 18:43:33 -0400

    Add parameters to new_rect_component and free_rect_component

--- rect_component.asm
+++ rect_component.asm
@@ -7,6 +7,23 @@
 ; // ==================================
 
 INCLUDE default_header.inc
+INCLUDE rect_component.inc
+INCLUDE heap_functions.inc
 
+.code
+new_rect_component PROC PUBLIC, h: DWORD, w : DWORD, r : BYTE, g : BYTE, b : BYTE, a : BYTE
+	mov eax, h
+	mov eax, w
+	mov al, r
+	mov al, g
+	mov al, b
+	mov al, a
+	ret
+new_rect_component ENDP
+
+free_rect_component PROC PUBLIC, pRect: DWORD
+	mov eax, pRect
+	ret
+free_rect_component ENDP
 
 END 
