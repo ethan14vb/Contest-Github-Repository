442880029f7928ab8b70e7781bca46b22f9c54b6
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-22 18:41:09 -0400

    Add rect_component.asm

---
+++ rect_component.asm
@@ -0,0 +1,12 @@
+; // ==================================
+; // RectComponent
+; // ----------------------------------
+; // Defines how a GameObject should appear using a rectangle.
+; // Should be used with a transform for proper functionality.
+; // The origin is defined by a transform.
+; // ==================================
+
+INCLUDE default_header.inc
+
+
+END 
