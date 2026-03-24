8b45bb9ba12cfb6b2aa2b867f2c11bdf8d3fa201
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-24 10:00:47 -0400

    Add rectangle_test_scene.asm

---
+++ rectangle_test_scene.asm
@@ -0,0 +1,12 @@
+; // ==================================
+; // rectangle_test_scene.asm
+; // ----------------------------------
+; // A scene to test the rendering of RectComponents.
+; // ==================================
+
+INCLUDE default_header.inc
+
+.data
+RectTestScene_Data LABEL BYTE
+
+END
