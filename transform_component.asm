7caffcc61956621627a7922c3716d4e542ebc397
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-23 21:54:05 -0400

    Changed transform_component.asm to have an init_transform_component method and a separate new_transform_component method

--- transform_component.asm
+++ transform_component.asm
@@ -10,15 +10,27 @@ INCLUDE transform_component.inc
 INCLUDE heap_functions.inc
 
 .code
-new_transform_component PROC PUBLIC, x: DWORD, y: DWORD, ignoreCamera: DWORD
-	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF TransformComponent
-
+; // ----------------------------------
+; // init_game_object
+; // Initializes memory with the contents of a TransformComponent
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
+init_transform_component PROC PUBLIC USES esi, x: DWORD, y : DWORD, ignoreCamera : DWORD
 	mov esi, x
-	mov (TransformComponent PTR [eax]).x, esi
+	mov (TransformComponent PTR [ecx]).x, esi
 	mov esi, y
-	mov (TransformComponent PTR [eax]).y, esi
+	mov (TransformComponent PTR [ecx]).y, esi
 	mov esi, ignoreCamera
-	mov (TransformComponent PTR [eax]).ignoreCamera, esi
+	mov (TransformComponent PTR [ecx]).ignoreCamera, esi
+	ret
+init_transform_component ENDP
+
+new_transform_component PROC PUBLIC USES ecx, x: DWORD, y: DWORD, ignoreCamera: DWORD
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF TransformComponent
+	mov ecx, eax ; // Move the memory address to eax so it can function as a "this" pointer
+	INVOKE init_transform_component, x, y, ignoreCamera
 
 	ret ; // Return with the address of the memory block in HeapAlloc
 new_transform_component ENDP
