4645b85b699b80c414017bddb1a19af810133741
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-24 19:49:12 -0400

    Added includes to game_object_test.asm

--- game_object_test.asm
+++ game_object_test.asm
@@ -7,12 +7,20 @@
 ; //	Exclude main.asm from the project and instead include this file, then build,
 ; // run, and feel free to debug and test.
 ; // ==================================
+
 INCLUDE default_header.inc
 INCLUDE heap_functions.inc
+INCLUDE transform_component.inc
+INCLUDE rect_component.inc
+INCLUDE game_object.inc
+
 ExitProcess PROTO STDCALL : DWORD
+
 .code
 main PROC PUBLIC
+	INVOKE initialize_heap
 	INVOKE ExitProcess, 0
 	ret
 main ENDP
+
 END main
