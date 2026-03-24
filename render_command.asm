e88856b99b6c1bca1c04685bc19e252157cbe0bb
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-23 22:49:26 -0400

    Add render_command.asm

---
+++ render_command.asm
@@ -0,0 +1,14 @@
+; // ==================================
+; // render_command.asm
+; // ----------------------------------
+; // A render command is meant to be compiled into
+; // a list of commands and passed to the renderer
+; // which will decide how exactly to render all of
+; // the commands passed to it.
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE render_command.inc
+INCLUDE heap_functions.inc
+
+END
