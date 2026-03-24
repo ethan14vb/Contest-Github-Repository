7cfdfdefbf8298a3026bdb1ff32f0c48faf4020e
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-24 11:18:29 -0400

    add another square to rectangle_test_scene.asm

--- rectangle_test_scene.asm
+++ rectangle_test_scene.asm
@@ -23,4 +23,27 @@ RectTestScene_Data LABEL BYTE ; // The label is set to BYTE so the data can be t
 		BYTE 0		; // b
 		BYTE 255	; // a
 
+		; // Component 2: TransformComponent
+		DWORD TRANSFORM_COMPONENT_ID
+		DWORD 10	; // x
+		DWORD 10	; // y
+		DWORD 0		; // ignoreCamera
+
+	; // GameObject 2: A blue square
+	DWORD 2 ; // Number of components
+		; // Component 1: RectComponent
+		DWORD RECT_COMPONENT_ID
+		DWORD 6		; // Height
+		DWORD 5		; // Width
+		BYTE 0		; // r
+		BYTE 0		; // g
+		BYTE 255	; // b
+		BYTE 255	; // a
+
+		; // Component 2: TransformComponent
+		DWORD TRANSFORM_COMPONENT_ID
+		DWORD 40	; // x
+		DWORD 10	; // y
+		DWORD 0		; // ignoreCamera
+
 END
