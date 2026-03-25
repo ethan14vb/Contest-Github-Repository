91b029902f6c3e9dfe4bacdf81bf5fbe252af4ae
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-24 22:50:10 -0400

    Implement constructor methods in component.asm

--- component.asm
+++ component.asm
@@ -16,4 +16,29 @@ INCLUDE default_header.inc
 INCLUDE heap_functions.inc
 INCLUDE component.inc
 
+.data
+COMPONENT_VTABLE Component_vtable <OFFSET free_component>
+
+.code
+init_component PROC PUBLIC USES esi
+	mov (Component PTR [ecx]).componentType, DEFAULT_COMPONENT_ID
+	mov (Component PTR [ecx]).pVt, OFFSET COMPONENT_VTABLE
+
+	ret
+init_component ENDP
+
+new_component PROC PUBLIC USES ecx
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF Component
+	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
+	INVOKE init_component
+
+	ret ; // Return with the address of the memory block in HeapAlloc
+new_component ENDP
+
+; // ecx stores the "this" pointer
+free_component PROC PUBLIC
+	INVOKE HeapFree, hHeap, 0, ecx
+	ret
+free_component ENDP
+
 END
