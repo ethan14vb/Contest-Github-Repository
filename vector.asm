6d07e501036daa481a774ca3e5fbd2c094801938
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-26 16:12:53 -0400

    Implemented init_vector

--- vector.asm
+++ vector.asm
@@ -13,16 +13,30 @@ INCLUDE vector.inc
 ; // ********************************************
 ; // Constructor Methods
 ; // ********************************************
-init_vector PROC, pData: DWORD, count : DWORD, capacity : DWORD
-	; // 
-	INVOKE init_vector, pData, count, capacity
+init_vector PROC USES esi, pData: DWORD, capacity : DWORD
+	; // Allocate space for the data
+	mov eax, capacity
+	shl eax, 2 ; // multiply capacity by 4, SIZEOF DWORD
+
+	push ecx
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, eax
+	pop ecx
+
+	mov (Vector PTR [ecx]).pData, eax
+
+	; // Initialize other member data
+	mov (Vector PTR [ecx]).count, 0
+	mov esi, capacity
+	mov (Vector PTR [ecx]).capacity, esi
+
+	mov eax, ecx ; // Return the this pointer
 	ret
 init_vector ENDP
 
-new_vector PROC USES ecx, pData: DWORD, count: DWORD, capacity: DWORD
+new_vector PROC USES ecx, pData: DWORD, capacity: DWORD
 	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF Vector
 	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
-	INVOKE init_vector, pData, count, capacity
+	INVOKE init_vector, pData, capacity
 
 	ret
 new_vector ENDP
