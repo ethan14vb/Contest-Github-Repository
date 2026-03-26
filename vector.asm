82114f625a921161e6c93882b4dd579f0fc949ee
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-26 15:58:24 -0400

    Added constructor methods to vector.asm

--- vector.asm
+++ vector.asm
@@ -9,6 +9,24 @@ INCLUDE default_header.inc
 INCLUDE vector.inc
 
 .code
+; // ********************************************
+; // Constructor Methods
+; // ********************************************
+init_vector PROC
+	ret
+init_vector ENDP
+
+new_vector PROC
+	ret
+new_vector ENDP
+
+free_vector PROC
+	ret
+free_vector ENDP
+
+; // ********************************************
+; // Instance methods
+; // ********************************************
 push_back PROC
 	ret
 push_back ENDP
