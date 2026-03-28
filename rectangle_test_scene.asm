68a90796e4a52049835bfb6cbdc6c9690ba193eb
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-28 16:30:23 -0400

    implemented populate_rectangle_test_scene

--- rectangle_test_scene.asm
+++ rectangle_test_scene.asm
@@ -7,6 +7,10 @@
 
 INCLUDE default_header.inc
 INCLUDE rectangle_test_scene.inc
+INCLUDE transform_component.inc
+INCLUDE rect_component.inc
+INCLUDE game_object.inc
+INCLUDE scene.inc
 
 .code
 ; // ----------------------------------
@@ -14,8 +18,35 @@ INCLUDE rectangle_test_scene.inc
 ; // Call this method on an empty Scene to fill it
 ; // with the rectangle test scene contents.
 ; // ----------------------------------
-populate_rectangle_test_scene PROC, pScene: DWORD
-	mov eax, pScene ; // Dummy instruction to avoid MASM assemble time errors
+populate_rectangle_test_scene PROC PUBLIC USES eax ebx edx esi edi, pScene: DWORD
+	; // Red Rectangle
+	INVOKE new_game_object, 2
+	mov ecx, eax
+
+	INVOKE new_transform_component, 10, 10, 0
+	INVOKE add_component, ecx, eax
+
+	INVOKE new_rect_component, 5, 6, 255, 0, 0, 255
+	INVOKE add_component, ecx, eax
+
+	mov esi, ecx
+	mov ecx, pScene
+	INVOKE instantiate_game_object, esi
+
+	; // Blue Rectangle
+	INVOKE new_game_object, 2
+	mov ecx, eax
+
+	INVOKE new_transform_component, 30, 10, 0
+	INVOKE add_component, ecx, eax
+
+	INVOKE new_rect_component, 6, 5, 0, 0, 255, 255
+	INVOKE add_component, ecx, eax
+
+	mov esi, ecx
+	mov ecx, pScene
+	INVOKE instantiate_game_object, esi
+
 	ret
 populate_rectangle_test_scene ENDP
 
