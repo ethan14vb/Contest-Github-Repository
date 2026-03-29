c6a18ef4596ee85fcab0b44315fdeb68a24fab47
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-28 20:18:00 -0400

    Add camera.asm

---
+++ camera.asm
@@ -0,0 +1,20 @@
+; // ==================================
+; // camera.asm
+; // ----------------------------------
+; // A camera defines where spatially a scene should
+; // render its GameObjects.
+; // ==================================
+INCLUDE default_header.inc
+.code
+init_camera PROC
+	ret
+init_camera ENDP
+
+new_camera PROC
+	ret
+new_camera ENDP
+
+free_camera PROC
+	ret
+free_camera ENDP
+END
