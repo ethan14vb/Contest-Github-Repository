5299cbd1af473259a8494de96b793972da75deef
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-28 23:17:55 -0400

    Add input_manager.asm

---
+++ input_manager.asm
@@ -0,0 +1,14 @@
+; // ==================================
+; // input_manager.asm
+; // ----------------------------------
+; // The input manager is responsible for determining
+; // which keys are currently pressed and providing
+; // a convenient method for other files to determine
+; // the currently pressed keys. This should be
+; // updated on a by-frame basis by a scene.
+; // ==================================
+
+INCLUDE default_header.inc
+
+
+END
