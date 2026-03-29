2c2f0c6ad41ad0bfce956cfefc0acb45e20e8290
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-29 00:07:08 -0400

    Add init_camera_mover_game_object

--- camera_mover_game_object.asm
+++ camera_mover_game_object.asm
@@ -11,7 +11,23 @@ INCLUDE game_object_ids.inc
 INCLUDE component.inc
 INCLUDE component_ids.inc
 INCLUDE heap_functions.inc
+
 .code
+; // ********************************************
+; // Constructor Methods
+; // ********************************************
+
+; // ----------------------------------
+; // init_camera_mover_game_object
+; // Initializes memory with the contents of a CameraMoverGameObject
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
+init_camera_mover_game_object PROC PUBLIC USES esi ebx edx
+	ret
+init_camera_mover_game_object ENDP
+
 camera_mover_update PROC stdcall, deltaTime: REAL4
 	mov eax, deltaTime
 	ret
