3914872fec60bbc4db6546fc2801999bfc865002
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-29 16:29:48 -0400

    Added populate_rectangle_test_scene to main.asm

--- main.asm
+++ main.asm
@@ -1,23 +1,39 @@
 ; // ==================================
 ; // main.asm
 ; // ----------------------------------
-; // The entry point of the program. Right now it is nonfunctional, but main.asm
-; // will be responsible for updating the currently running scene and the inialization
-; // of the engine.
+; // The entry point of the program.
+; // main.asm is responsible for updating the currently running scene 
+; // and the inialization of the engine.
 ; // ==================================
 
 INCLUDE default_header.inc
 INCLUDE heap_functions.inc
+INCLUDE rectangle_test_scene.inc
+INCLUDE scene.inc
 
 ExitProcess PROTO : DWORD
 
+.data
+deltaTime REAL4 0.03333
+
 .code
 main PROC PUBLIC
-	; // Eventually, this is where the main loop and heart of the engine will be.
-	; // This loop will update at a fixed 30 frames per second and call scene_update
-	; // on the current scene.
+	local pScene
+
 	INVOKE initialize_heap
+
+	INVOKE new_scene, 2
+	mov pScene, eax
+	
+	INVOKE populate_rectangle_test_scene, pScene
+
+	mov ecx, pScene
+	INVOKE scene_update, deltaTime
+
+	INVOKE free_scene
+
 	INVOKE ExitProcess, 0
+	ret
 main ENDP
 
 END main
