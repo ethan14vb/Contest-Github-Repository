c06938e85ab79a23dca812d19901ecc2bbf4827a
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-30 23:22:24 -0400

    Add start of neon_square_player_update

--- neon_square_player.asm
+++ neon_square_player.asm
@@ -11,6 +11,9 @@ INCLUDE scene.inc
 INCLUDE heap_functions.inc
 INCLUDE input_manager.inc
 
+.data
+NEON_SQUARE_PLAYER_GAMEOBJECT_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET neon_square_player_update, OFFSET game_object_exit>
+
 .code
 ; // ********************************************
 ; // Constructor Methods
@@ -27,9 +30,16 @@ init_neon_square_player PROC PUBLIC USES esi ebx edx
 	; // Parent constructor
 	INVOKE init_game_object, 2
 	;mov (GameObject PTR [ecx]).gameObjectType, NEON_SQUARE_PLAYER_GAME_OBJECT_ID
-	;mov (GameObject PTR [ecx]).pVt, OFFSET NEON_SQUARE_PLAYER_GAMEOBJECT_VTABLE
+	mov (GameObject PTR [ecx]).pVt, OFFSET NEON_SQUARE_PLAYER_GAMEOBJECT_VTABLE
 		
 	ret
 init_neon_square_player ENDP
+
+neon_square_player_update PROC stdcall USES eax, deltaTime: REAL4
+	local pThis : DWORD
+	mov pThis, ecx
+	mov eax, deltaTime ; // Use the deltaTime variable so MASM doesn't get angry and throw a compile time error
+	ret
+neon_square_player_update ENDP
 
 END 
