d33fcd2afbd2b66860840996bcf74029a711896c
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-30 21:57:19 -0400

    Added header comments to NeonSquareScene

--- neon_square_scene.asm
+++ neon_square_scene.asm
@@ -1,5 +1,11 @@
+; // ==================================
+; // neon_square_scene.asm
+; // ----------------------------------
+; // The main scene of our demo game for our engine: Neon Square
+; // ==================================
 
 INCLUDE default_header.inc
+
 .code
 populate_neon_square_scene PROC PUBLIC USES eax ebx edx esi edi, pScene: DWORD
 	mov eax, pScene
