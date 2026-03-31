72ef44af60bee63ee6c0522770addb736076eb14
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-30 23:20:21 -0400

    Implement init_neon_square_player

--- neon_square_player.asm
+++ neon_square_player.asm
@@ -11,6 +11,25 @@ INCLUDE scene.inc
 INCLUDE heap_functions.inc
 INCLUDE input_manager.inc
 
+.code
+; // ********************************************
+; // Constructor Methods
+; // ********************************************
 
+; // ----------------------------------
+; // init_neon_square_player
+; // Initializes memory with the contents of a NeonSquarePlayer
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
+init_neon_square_player PROC PUBLIC USES esi ebx edx
+	; // Parent constructor
+	INVOKE init_game_object, 2
+	;mov (GameObject PTR [ecx]).gameObjectType, NEON_SQUARE_PLAYER_GAME_OBJECT_ID
+	;mov (GameObject PTR [ecx]).pVt, OFFSET NEON_SQUARE_PLAYER_GAMEOBJECT_VTABLE
+		
+	ret
+init_neon_square_player ENDP
 
 END 
