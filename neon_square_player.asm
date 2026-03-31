8124cc9f8ac5ca8f95dae1ed2f5a06f8f6eb2bab
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-03-30 23:15:45 -0400

    Add neon_square_player.asm

---
+++ neon_square_player.asm
@@ -0,0 +1,16 @@
+; // ==================================
+; // neon_square_player.asm
+; // ----------------------------------
+; // The NeonSquarePlayer is a subclass of GameObject that functions
+; // as the player in the NeonSquareScene.
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE game_object.inc
+INCLUDE scene.inc
+INCLUDE heap_functions.inc
+INCLUDE input_manager.inc
+
+
+
+END 
--- neon_square_scene.asm
+++ neon_square_scene.asm
@@ -8,7 +8,11 @@ INCLUDE default_header.inc
 
 .code
 populate_neon_square_scene PROC PUBLIC USES eax ebx edx esi edi, pScene: DWORD
+	local pThis : DWORD
+	mov pThis, ecx
+	; // Player Square
 	mov eax, pScene
+
 	ret
 populate_neon_square_scene ENDP
 
