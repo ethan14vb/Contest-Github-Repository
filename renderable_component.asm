1ef0f3ec818d31d14ce92cee3a7a87ae6e13aca9
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-01 18:33:20 -0400

    Added renderable_component.asm

---
+++ renderable_component.asm
@@ -0,0 +1,19 @@
+; // ==================================
+; // renderable_component.asm
+; // ----------------------------------
+; // A subclass of component that provides
+; // fields for the RenderableComponents which
+; // are RectComponents and SpriteComponents
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE renderable_component.inc
+INCLUDE heap_functions.inc
+
+
+
+
+
+
+
+END 
