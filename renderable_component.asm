3c5aa7ab6aef52fb43a546ea9ebb50ed5bff58cd
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-01 18:37:09 -0400

    Implemented init_renderable_component

--- renderable_component.asm
+++ renderable_component.asm
@@ -8,12 +8,30 @@
 
 INCLUDE default_header.inc
 INCLUDE renderable_component.inc
+INCLUDE component_ids.inc
 INCLUDE heap_functions.inc
 
+.code
+; // ----------------------------------
+; // init_renderable_component
+; // Initializes memory with the contents of a RenderableComponent
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
+init_renderable_component PROC PUBLIC USES esi, visible : DWORD, layer : DWORD
+	; // Parent constructor
+	INVOKE init_component
+	mov (Component PTR [ecx]).componentType, RENDERABLE_COMPONENT_ID
 
+	; // Height & Width
+	mov esi, visible
+	mov (RenderableComponent PTR [ecx]).visible, esi
+	mov esi, layer
+	mov (RenderableComponent PTR [ecx]).layer, esi
 
-
-
-
+	mov eax, ecx
+	ret
+init_renderable_component ENDP
 
 END 
