0689962327c73f5425ffc6031ac7f5f2372f5ef8
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-02 01:21:13 -0400

    Added explosion_game_object.asm

---
+++ explosion_game_object.asm
@@ -0,0 +1,10 @@
+; // ==================================
+; // explosion_game_object.asm
+; // ----------------------------------
+; // Appears large and opaque at first before fading away.
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE explosion_game_object.inc
+
+END
