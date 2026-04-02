46aa7fa2b4123a133a26a2b09fb85a750efb7833
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-02 01:23:15 -0400

    Implemented init_explosion_game_object

--- explosion_game_object.asm
+++ explosion_game_object.asm
@@ -6,5 +6,44 @@
 
 INCLUDE default_header.inc
 INCLUDE explosion_game_object.inc
+INCLUDE transform_component.inc
+INCLUDE rect_component.inc
+
+.data
+EXPLOSION_GAMEOBJECT_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET game_object_update, OFFSET game_object_exit, OFFSET free_game_object>
+
+.code
+; // ********************************************
+; // Constructor Methods
+; // ********************************************
+
+; // ----------------------------------
+; // init_neon_square_player
+; // Initializes memory with the contents of a NeonSquarePlayer
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
+init_explosion_game_object PROC PUBLIC USES esi ebx edx
+	local pThis
+	mov pThis, ecx
+
+	; // Parent constructor
+	INVOKE init_game_object, 2
+	mov (GameObject PTR [ecx]).gameObjectType, EXPLOSION_GAME_OBJECT_ID
+	mov (GameObject PTR [ecx]).pVt, OFFSET EXPLOSION_GAMEOBJECT_VTABLE
+
+	; // Add transform component
+	INVOKE new_transform_component, 20, 25, 0
+	INVOKE add_component, pThis, eax
+
+	; // Add rect component
+	INVOKE new_rect_component, 2, 2, 0, 255, 0, 255
+	INVOKE add_component, pThis, eax
+
+	mov eax, pThis
+		
+	ret
+init_explosion_game_object ENDP
 
 END
