b092427e93fc4c38db2e737f0b17a2769ab485cc
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-02 08:40:53 -0400

    Implemented init_background_rect_game_object fully

--- background_rect_game_object.asm
+++ background_rect_game_object.asm
@@ -7,6 +7,10 @@
 INCLUDE default_header.inc
 INCLUDE game_object.inc
 INCLUDE game_object_ids.inc
+INCLUDE background_rect_game_object.inc
+INCLUDE transform_component.inc
+INCLUDE rect_component.inc
+INCLUDE renderable_component.inc
 
 .data
 BACKGROUND_RECT_GAMEOBJECT_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET game_object_update, OFFSET game_object_exit, OFFSET free_game_object>
@@ -16,7 +20,7 @@ BACKGROUND_RECT_GAMEOBJECT_VTABLE GameObject_vtable <OFFSET game_object_start, O
 ; // Constructor Methods
 ; // ********************************************
 
-init_background_rect_game_object PROC PUBLIC USES esi ebx edx
+init_background_rect_game_object PROC PUBLIC USES esi ebx edx, x : DWORD, y : DWORD, h : DWORD, w : DWORD, r : BYTE, g : BYTE, b : BYTE, a : BYTE, layer : DWORD, speed : DWORD
 	local pThis
 	mov pThis, ecx
 
@@ -24,6 +28,20 @@ init_background_rect_game_object PROC PUBLIC USES esi ebx edx
 	INVOKE init_game_object, 2
 	mov (GameObject PTR [ecx]).gameObjectType, BACKGROUND_RECT_GAME_OBJECT_ID
 	mov (GameObject PTR [ecx]).pVt, OFFSET BACKGROUND_RECT_GAMEOBJECT_VTABLE
+
+	; // My constructor
+	mov esi, speed
+	mov (BackgroundRect PTR [ecx]).speed, esi
+		
+	; // Add transform component
+	INVOKE new_transform_component, x, y, 0
+	INVOKE add_component, pThis, eax
+
+	; // Add rect component
+	INVOKE new_rect_component, h, w, r, g, b, a
+	mov esi, layer
+	mov (RenderableComponent PTR [eax]).layer, esi
+	INVOKE add_component, pThis, eax
 
 	mov eax, pThis
 		
